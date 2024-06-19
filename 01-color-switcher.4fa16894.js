62466e7a60fe174f79fd4de613137e840d76ad0f
chanome2 <146432634+chanome2@users.noreply.github.com>
2024-06-19 19:29:49 +0000

    Deploying to gh-pages from @ chanome2/chanome2-goit-js-hw-09@ecb948dad60955d99169b33453f8379817bd87a0 🚀

---
+++ 01-color-switcher.4fa16894.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n),n=null}))}));
+//# sourceMappingURL=01-color-switcher.4fa16894.js.map
---
+++ 01-color-switcher.5b4efb5b.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n),n=null}))}));
+//# sourceMappingURL=01-color-switcher.5b4efb5b.js.map
